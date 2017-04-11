dea06616d13c196215f65215a1db5a7af2c551e3
Lou Lee <louisa.leeyh@gmail.com>
2017-04-11 10:13:00 +0800

    trying to get heroku to work

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // express set up
 var express = require('express')
 var app = express()
-var port = process.env.PORT || 4500
+var port = process.env.PORT || 7777
 
 // mongoose setup
 var dbURI = process.env.PROD_MONGODB || 'mongodb://localhost:27017/mymdb'
 var mongoose = require('mongoose')
 mongoose.connect(dbURI)
 
+var ejsLayouts = require('express-ejs-layouts')
 app.set('view engine', 'ejs')
+app.use(ejsLayouts)
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
