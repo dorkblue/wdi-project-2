46aa5d5c98dd4b1fdfec1f2396fdc71a0ec45f2a
Lou Lee <louisa.leeyh@gmail.com>
2017-04-16 14:54:14 +0800

    search is working

--- routers/item_router.js
+++ routers/item_router.js
@@ -31,7 +31,8 @@ router.post('/', function(req, res) {
   Item.find( { tag: req.body.search, user_id: req.user._id }, (err, output) => {
     if (err) res.send(err)
     res.render('index', {
-      foundItems: output
+      allItems: output,
+      userName: req.user.username
     })
   })
 })
