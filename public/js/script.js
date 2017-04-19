887439c0dad6a528f140748cf6361433c7bd4210
Lou Lee <louisa.leeyh@gmail.com>
2017-04-19 11:19:19 +0800

    added sakura petals :3

--- public/js/script.js
+++ public/js/script.js
@@ -1,14 +1,6 @@
-  var modal = document.getElementById('myModal');
-  var btn = document.getElementById("myBtn");
-  var span = document.getElementsByClassName("close")[0];
-  instructions.onclick = function() {
-      modal.style.display = "block";
-  }
-  span.onclick = function() {
-      modal.style.display = "none";
-  }
-  window.onclick = function(event) {
-      if (event.target == modal) {
-          modal.style.display = "none";
-      }
-  }
+$(document).ready(function() {
+  $(window).load(function() {
+    console.log('sakura');
+    $('body').sakura();
+  });
+});
